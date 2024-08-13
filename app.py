78a6bbe310fe0e5e7b9a2cf007bd804f04aec93f
Sambhav Mehta <58766591+sambhavm22@users.noreply.github.com>
2024-08-14 01:02:13 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Configure OpenAI API key
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context. Make sure to provide all the details. If the answer is not in
+    the provided context, just say, "The answer is not available in the context". Do not provide a wrong answer.\n\n
+    Context:\n {context}?\n
+    Question:\n {question}\n
+    Answer:
+    """
+    model = ChatOpenAI(temperature=0.3, openai_api_key=openai_api_key)
+
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+def user_input(user_question):
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    
+    # Allow dangerous deserialization, but only do this if you trust the source of the file
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+    
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True
+    )
+
+    st.write("Reply: ", response["output_text"])
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using OpenAI💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+if __name__ == "__main__":
+    main()
